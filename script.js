841382adf669d466e7e92681f9446353c7619c42
blakegspears <110437464+blakegspears@users.noreply.github.com>
2022-08-22 19:03:08 -0500

    JSassignment1
    added 20 variables based on website.

---
+++ script.js
@@ -0,0 +1,127 @@
+//alert("First JS Class");
+//console.log("Hello, first JS class");
+//console.error("Error! The service is not connected");
+//console.warn("Warning! The service is not connected");
+
+/*var userName="Blake"; //old way
+userName="Spears"
+let age=30;//defining the var age
+age=31;
+const taxes=11;//cannot change constant values
+
+console.log(userName);
+console.log(age);
+console.log(taxes);
+
+//document.write("<p> Name: " + userName + "</p><p> Age: " + age + "</p><p> Taxes: " + taxes + "</p");
+
+document.write(
+    <p>Name: ${userName} </p>
+    <p>Age: ${age} </p>
+    <p>Taxes: ${taxes} </p>
+
+);
+
+let children= 1;
+var partnerName="Mandee";
+var geoLocation="Elk River, MN";
+var jobTitle="Software Engineer";
+let myName="Blake Spears"
+myName=true;
+let email= "bgs2392@gmail.com";
+let password= "password";
+let age= 30;
+var country= "USA";
+let salary= 10000;
+let annualSalary=salary*12;
+
+console.log(children);
+console.log(partnerName);
+console.log(geoLocation);
+console.log(jobTitle);
+console.log(myName);
+console.log(email);
+console.log(password);
+console.log(age);
+console.log(country);
+console.log(salary);
+
+document.write(
+    "I will be a " + jobTitle + " in " + geoLocation + ", and married to " + partnerName + " with " + children + " kid."
+);
+
+document.write(`
+    <p>Name: ${myName} </p>
+    <p>Age: ${age} </p>
+    <p>Email: ${email} </p>
+    <p>Password: ${password} </p>
+    <p>Annual Salary: ${annualSalary} </p>
+    <p>Country: ${country} </p>
+`)
+*/
+
+let store= "Amazon";
+var title= "Best Seller";
+var department= "Electronics";
+var subDepartment= "Security & Surveillance";
+let topic= "Surveillance Cameras";
+let subTopic= "Bullet Cameras";
+let cameraName= "Wyze Cam v3";
+let cameraRating= "#1";
+var cameraReviews= 61926;
+let cameraStyle1= "Wyze Cam v3";
+let style1Price= 35.98;
+let cameraStyle2= "Wyze Cam v3 + Cam Plus";
+let style2Price= 35.98;
+let cameraStyle3= "Wyze Cam v3 2 Pack";
+let style3Price= 70.96;
+let delivery= "Prime Overnight";
+let primeSavings= 10.00;
+let expertAssembly= 119.99;
+let protectionPlan2Yr= 2.99;
+let protectionPlan3Yr= 4.99;
+
+console.log(store);
+console.log(title);
+console.log(department);
+console.log(subDepartment);
+console.log(topic);
+console.log(subTopic);
+console.log(cameraName);
+console.log(cameraRating);
+console.log(cameraReviews);
+console.log(cameraStyle1);
+console.log(style1Price);
+console.log(cameraStyle2);
+console.log(style2Price);
+console.log(cameraStyle3);
+console.log(style3Price);
+console.log(delivery);
+console.log(primeSavings);
+console.log(expertAssembly);
+console.log(protectionPlan2Yr);
+console.log(protectionPlan3Yr);
+
+document.write(`
+    <p>Website: ${store} </p>
+    <p>Title: ${title} </p>
+    <p>Department: ${department} </p>
+    <p>SubDepartment: ${subDepartment} </p>
+    <p>Topic: ${topic} </p>
+    <p>SubTopic: ${subTopic} </p>
+    <p>Camera: ${cameraName} </p>
+    <p>CameraRating: ${cameraRating} </p>
+    <p>CameraReviews: ${cameraReviews} </p>
+    <p>CameraStyle1: ${cameraStyle1} </p>
+    <p>Price: ${style1Price} </p>
+    <p>CameraStyle2: ${cameraStyle2} </p>
+    <p>Price: ${style2Price} </p>
+    <p>CameraStyle3: ${cameraStyle3} </p>
+    <p>Price: ${style3Price} </p>
+    <p>Delivery: ${delivery} </p>
+    <p>Prime Savings: ${primeSavings} </p>
+    <p>Expert Assembly: ${expertAssembly} </p>
+    <p>2 Year Protection Plan: ${protectionPlan2Yr} </p>
+    <p>3 Year Protection Plan: ${protectionPlan3Yr} </p>
+`)
+
